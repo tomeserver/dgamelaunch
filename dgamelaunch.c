9a98008db6c2ce3871b9e7b9c2c0295f1da0f4db
Adam Reiser <reiser@defensivecomputing.io>
2016-01-30 22:53:48 -0500

    Merge Neil Moore's password salting fixes

--- dgamelaunch.c
+++ dgamelaunch.c
@@ -90,6 +90,7 @@
 #include <signal.h>
 #include <assert.h>
 #include <ctype.h>
+#include <inttypes.h>
 #include <unistd.h>
 #include <termios.h>
 
@@ -1485,21 +1486,81 @@ change_email ()
   }
 }
 
+char
+base64 (int value)
+{
+  int digit = value & 0x3f;
+
+  if (digit < 2)
+    return digit + '.';
+
+  digit -= 2;
+  if (digit < 10)
+    return digit + '0';
+
+  digit -= 10;
+  if (digit < 26)
+    return digit + 'A';
+
+  digit -= 26;
+  return digit + 'a';
+}
+
+const char *
+make_salt (void)
+{
+  static char salt[DGL_SALTLEN + 1];
+  static char saltstr[DGL_SALTLEN + 6];
+  uint32_t randoms[DGL_SALT_WORDS];
+  size_t nread = 0;
+  FILE *urand = fopen("/dev/urandom", "r");
+  if (!urand) {
+    debug_write("opening /dev/urandom");
+    graceful_exit(124);
+  }
+
+  nread = fread(randoms, sizeof(uint32_t), DGL_SALT_WORDS, urand);
+  if (nread != DGL_SALT_WORDS) {
+    debug_write("short read from /dev/urandom");
+    graceful_exit(125);
+  }
+  fclose(urand);
+
+  if (*DGL_PWALG == '\0') {
+    salt[0] = base64(randoms[0]);
+    salt[1] = base64(randoms[1]);
+    salt[2] = '\0';
+    return salt;
+  }
+
+  int bits = 0;
+  int idx = 0;
+
+  while (bits < DGL_SALTLEN * 6) {
+    int firstbit = bits & 0x1f;
+    int lastbit = (bits + 6) & 0x1f;
+    unsigned val = randoms[bits/32];
+
+    // including a few from the next word
+    if (lastbit > firstbit) {
+      val >>= (32 - lastbit);
+    } else {
+      val <<= 6 - (32 - firstbit);
+      val |= randoms[bits/32 + 1] >> (32 - lastbit);
+    }
+
+    salt[bits / 6] = base64(val);
+    bits += 6;
+  }
+  salt[bits/6] = '\0';
+  sprintf(saltstr, "$%s$%s$", DGL_PWALG, salt);
+  return saltstr;
+}
+
 int
 changepw (int dowrite)
 {
   char buf[DGL_PASSWDLEN+1];
-  unsigned long seed[2];
-  char salt[8+3+1] = "$6$";
-  int i;
-  const char *const seedchars =
-		    "./0123456789ABCDEFGHIJKLMNOPQRST"
-			    "UVWXYZabcdefghijklmnopqrstuvwxyz";
-  seed[0] = time(NULL);
-  seed[1] = getpid() ^ (seed[0] >> 14 & 0x30000);
-  for (i = 0; i < 8; i++)
-	  salt[3+i] = seedchars[(seed[i/5] >> (i%5)*6) & 0x3f];
-  salt[9] = '\0';
   int error = 2;
 
   /* A precondition is that struct `me' exists because we can be not-yet-logged-in. */
@@ -1565,7 +1626,7 @@ changepw (int dowrite)
     }
 
   free(me->password);
-  me->password = strdup (crypt (buf, salt));
+  me->password = strdup (crypt (buf, make_salt()));
 
   if (dowrite)
     writefile (0);
@@ -2062,26 +2123,16 @@ newuser ()
 int
 passwordgood (char *cpw)
 {
-  char *crypted;
-
   assert (me != NULL);
 
-  /* Calculate the hash, getting the salt from the known hash */
-  crypted = crypt (cpw, me->password);
+  if (!strncmp (crypt (cpw, me->password), me->password, DGL_CRYPTLEN))
+    return 1;
+  if (!strncmp (cpw, me->password, DGL_CRYPTLEN))
+    return 1;
 
-  if (crypted == NULL)
-      return 0;
-
-  /* Try sha512 */
-  if (!strncmp (crypted, me->password, 1024))
-	  return 1;
-  else
-  {
-	/* Otherwise use the default */
-	crypted = crypt (cpw, cpw);
-	if (!strncmp (crypted, me->password, 1024))
-		return 1;
-  }
+  /* Compatibility */
+  if (!strncmp (cpw, cpw, DGL_CRYPTLEN))
+    return 1;
 
   return 0;
 }
@@ -2133,7 +2184,7 @@ readfile (int nolock)
       users[f_num] = malloc (sizeof (struct dg_user));
       users[f_num]->username = (char *) calloc (DGL_PLAYERNAMELEN+2, sizeof (char));
       users[f_num]->email = (char *) calloc (82, sizeof (char));
-      users[f_num]->password = (char *) calloc (DGL_PASSWDLEN+2, sizeof (char));
+      users[f_num]->password = (char *) calloc (DGL_CRYPTLEN+1, sizeof (char));
       users[f_num]->env = (char *) calloc (1026, sizeof (char));
 
       /* name field, must be valid */
@@ -2173,7 +2224,7 @@ readfile (int nolock)
         {
           users[f_num]->password[(b - n)] = *b;
           b++;
-          if ((b - n) >= DGL_PASSWDLEN) {
+          if ((b - n) >= DGL_CRYPTLEN) {
 	      debug_write("passwd field too long");
             graceful_exit (102);
 	  }
